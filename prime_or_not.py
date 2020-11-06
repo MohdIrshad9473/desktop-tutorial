746f9650368731ceaf009550e304fbffce3d942b
Mohd irshad <66816040+MohdIrshad9473@users.noreply.github.com>
2020-11-06 09:52:35 -0800

    update prime number

--- prime_or_not.py
+++ prime_or_not.py
@@ -1,13 +1,16 @@
 def prime(a):
+    ''' prime number of variable
+        :param: int a
+    '''
     if a > 1:
-        for i in range(2, a):
+        for i in range(2, int(a/2)):
             if a % i == 0:
-                print("not prime number")
-                break
-        else:
-            print("prime number")
-    else:
-        print("not prime number")
+                return False
+        return True
+    return False
 
 
-print(prime(int(input("Enter any number :"))))
+if prime(int(input("Enter any number :"))):
+    print("  prime number")
+else:
+    print("not prime number")
